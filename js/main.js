f168dc18da959a1f422a761108a1335435bb8c8c
vitaliyterziev <vitaliyterziev@gmail.com>
2015-11-16 21:47:01 +0200

    wording

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var SnakeGame = {
 
     setup: function() {
         'use strict'
-        // initial variables and document objects
+        // initial variables
         var canvas = document.getElementById('basic'),
             ctx = canvas.getContext('2d'),
             gameStat = document.querySelectorAll('h3')[0],
